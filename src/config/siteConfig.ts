2ea52662c2a1e7c32fe36655a8731d77bb08ad46
luoye-duck <luoye6213@gmail.com>
2026-07-15 16:37:57 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -14,6 +14,8 @@ export const siteConfig: SiteConfig = {
 	// 站点 URL
 	site_url: "https://luoyebk.cc.cd",
 
+	备用site_url: "https://luoyebk.eu.cc",
+
 	// 站点描述
 	description:
 		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
